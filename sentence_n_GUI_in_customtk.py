e298736e11958656db8b7494a022eeabc5fe94ea
Abolfazl Darabi <darabia88@gmail.com>
2025-01-11 08:09:23 +0330

    End of creating gui with custom tkinter
    Signed-off-by: Abolfazl Darabi <darabia88@gmail.com>

--- sentence_n_GUI_in_customtk.py
+++ sentence_n_GUI_in_customtk.py
@@ -1,5 +1,30 @@
 from customtkinter import *
 
+
+#تابع محاسبات
+def Operation():
+    #گرفتن اطلاعات و تبدیل به لیست
+    sequence = list(input_.get())
+    # حذف فاصله‌ها از لیست
+    sequence = [x for x in sequence if x != " "]
+    # تبدیل اعضای لیست به نوع داده‌ای int
+    sequence = list(map(int, sequence))
+
+    a1 = sequence[0]
+    a2 = sequence[1]
+    a3 = sequence[2]
+    # انجام محاسبات
+    x1 = a1 + a3
+    x2 = a2 * 2
+    # بررسی برابری x1 و x2 و محاسبه d
+    if x1 == x2:
+        d = a2 - a1
+        answer.configure(text=f"tn = {a1} + (n-1) * {d}")
+    else:
+        answer.configure(text="Your sequence does not count")
+        
+        
+        
 #ساخت یک پنجره
 win = CTk()
 #تایتل پنجره
@@ -15,10 +40,10 @@ guide.pack(pady=(5,0))
 input_= CTkEntry(win, 240, 35, font=CTkFont(size=16),placeholder_text="Enter your sequence:")
 input_.pack(pady=(10,5))
 #دکمه ارسال اطلاعات
-submit = CTkButton(win, 70, 35,text="submit",font=CTkFont(size=16), corner_radius=12)
+submit = CTkButton(win, 70, 35,text="submit",font=CTkFont(size=16), corner_radius=12, command=Operation)
 submit.pack()
 #بخش نشان دهنده جواب
-answer = CTkLabel(win, 240, 40, text=" ")
+answer = CTkLabel(win, 240, 40, text=" ",font=CTkFont(size=20))
 answer.pack(pady=10)
 #اجرا پنجره
 win.mainloop()
